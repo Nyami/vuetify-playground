f2832d38ac7acdb9ba88a624b1656997aa72af70
Douglas Cameron <nyami@hotmail.com>
2019-09-23 10:27:50 +0100

    Added vuetify with IE 11 support

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'babel-polyfill';
+
 import Vue from 'vue';
 import App from './App.vue';
 import vuetify from './plugins/vuetify';
@@ -6,5 +8,5 @@ Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
-  render: (h) => h(App)
+  render: (h) => h(App),
 }).$mount('#app');
